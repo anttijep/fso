56ce2dd75b977acfae2cfa3de8961bd2f27fed46
Antti <antti.vas@gmail.com>
2022-05-20 13:04:39 +0300

    2.1.2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Number = ({name, number}) => <p>{name} {number}</p>
 const Filter = ({target, handler}) => <div>filter shown with <input value={target} onChange={handler}/></div>
@@ -15,15 +16,15 @@ const NumForm = ({newName, newNameHandler, newNumber, newNumberHandler, submitHa
   </form>
 
 const App = () => {
-   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(r => setPersons(r.data))
+  },[]);
+
 
   const click = (e) => {
     e.preventDefault();
