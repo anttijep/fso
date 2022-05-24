504f3736070a4e289fdc7afe063a971a8030d094
Antti <>
2022-05-24 09:46:29 +0000

    deploy should work..

--- osa3/puhelinluettelo_frontend/src/services/phonebook.js
+++ osa3/puhelinluettelo_frontend/src/services/phonebook.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 
-const domain = 'http://localhost:3001/api/persons';
+const domain = 'api/persons';
 
 const getAll = () => {
     return axios.get(domain).then(r => r.data);
