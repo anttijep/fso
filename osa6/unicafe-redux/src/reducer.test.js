a2ff5a14eb459c5cc782d8ad9e8da7058976bc1c
Antti <antti.vas@gmail.com>
2022-06-02 17:28:18 +0300

    osa6 alku

---
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -0,0 +1,94 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+  test('reset works', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const action2 = {
+      type: 'OK'
+    }
+    const action3 = {
+      type: 'BAD'
+    }
+    const action4 = {
+      type: "RESET"
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    let newState = counterReducer(state, action)
+    newState = counterReducer(newState, action2)
+    newState = counterReducer(newState, action3)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 1
+    })
+    newState = counterReducer(state, action4);
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+})
